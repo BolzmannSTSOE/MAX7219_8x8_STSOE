3bc5abd1b6ae7b55ade4731e19e1a9c4f9547155
BolzmannSTSOE <andy.bolzmann@stsoe.hamburg.de>
2025-12-25 20:20:33 +0100

    Update MAX7219.ts

--- MAX7219.ts
+++ MAX7219.ts
@@ -52,8 +52,8 @@ namespace max7219_matrix {
 
     // set micro:bit SPI
     pins.spiPins(mosi, miso, sck)
-    pins.spiFormat(8, 3)
-    // pins.spiFormat(8, 0)
+    //pins.spiFormat(8, 3)
+    pins.spiFormat(8, 0)
     pins.spiFrequency(100000)
     // initialize MAX7219s
     _registerAll(_SHUTDOWN, 0) // turn off
@@ -1217,6 +1217,7 @@ enum flip_direction {
   //% block.loc.de="vertikal"
   vertical = 2
 }
+
 
 
 
